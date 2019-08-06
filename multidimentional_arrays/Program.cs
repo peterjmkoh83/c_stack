e3b75c63d8b0bcae3b9107019b6e819b8f110395
Peter Koh <jmkoh83@gmail.com>
2019-08-06 12:20:33 -0700

    updated

---
+++ multidimentional_arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace multidimentional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiplication = new int [10,10];
+            
+            for (int i = 0; i < multiplication.GetLength(0); i++) 
+            {
+                int num = 1;
+                for (int j = 0; j < multiplication.GetLength(1); j++)
+                {
+                    multiplication[i,j] = num * (i + 1);
+                    num++;
+                    System.Console.WriteLine(multiplication[i,j]);
+                }
+            }            
+        }
+    }
+}
