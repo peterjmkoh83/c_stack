1d7396612757a1e0266a2c0ae3f204b5107851dd
Peter Koh <jmkoh83@gmail.com>
2019-08-06 13:36:07 -0700

    updated

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> lists = new List<object>();
+            lists.Add(7);
+            lists.Add(28);
+            lists.Add(-1);
+            lists.Add(true);
+            lists.Add("Chair");
+
+            int sum = 0;
+            foreach(var list in lists) 
+            {
+                if (list is int) {
+                    System.Console.WriteLine("Integer: " + list);
+                    sum += (int)list;
+
+                }
+                if (list is bool) {
+                    System.Console.WriteLine("Boolean: " + list);
+                }
+                if (list is string) {
+                    System.Console.WriteLine("String: " + list);
+                }
+            }
+            System.Console.WriteLine("Sum of all integers: " + sum);
+        }
+    }
+}
