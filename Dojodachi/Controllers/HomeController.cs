be7a6eb151e0d7a0ca94009209a42fa7e4de56ea
Peter Koh <jmkoh83@gmail.com>
2019-08-16 11:50:28 -0700

    working on dojodachi

---
+++ Dojodachi/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dojodachi.Models;
+
+
+namespace Dojodachi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("lucian")]
+        public IActionResult Lucian()
+        {
+           return View("Lucian");
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
